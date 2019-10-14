de3413dea725d1acf71c5512fd8faa40ed4496db
pauling2 <yzz664312431@126.com>
2019-10-14 11:01:22 +0800

    reset from onenote

---
+++ RegPhos.py
@@ -0,0 +1,15 @@
+#coding=utf8
+
+
+##对regphos文件的处理
+
+with open(r'E:\python_project\original_data\RegPhos.fasta')as f:
+    lst=f.readlines()[1:];sub=[];x=0
+    for i in lst:
+        if i.split('\t')[4]:
+            x+=1
+            # print i.split('\t')[4]
+            if i.split('\t')[1] not in sub:
+                sub.append(i.split('\t')[1])
+    print x;print len(sub)
+f.close()
